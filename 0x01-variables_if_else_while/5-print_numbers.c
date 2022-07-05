39b49c6baa9b072a67ec5510069766f5284b3243
wand3 <godswillbaiyewu@gmail.com>
2022-07-05 06:07:11 -0700

    Print numbers 0-9 commit

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * prints all singe digit numbers of base 10 from 0
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	int num;
+
+
+	for (num = 0; num < 10; num++)
+	{
+		printf("%d", num);
+	}
+	printf("\n");
+
+	return (0);
+}
