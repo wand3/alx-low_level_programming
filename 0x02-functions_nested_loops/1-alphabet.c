af4615bc5f08e0a0beeb7981d2426a73b148f722
wand3 <godswillbaiyewu@gmail.com>
2022-07-10 07:12:17 -0700

    print alphabet in lowecase using _putchar

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * print_alphabet - print lowercase alphabet followed by newline
+ * Return: Always 0 (success)
+ */
+void print_alphabet(void)
+{
+	char letter;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+		_putchar(letter);
+	_putchar('\n');
+}
