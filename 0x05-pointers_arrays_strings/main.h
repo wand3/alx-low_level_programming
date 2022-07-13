67c655bb48ca05c66eb4c6600e33ee5d407f0095
wand3 <godswillbaiyewu@gmail.com>
2022-07-12 19:19:30 -0700

    Prints a string commit

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -2,9 +2,11 @@
 #define MAIN_H
 #include <stdlib.h>
 
+
 int _putchar(char c);
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
 int _strlen(char *s);
+void _puts(char *str);
 
 #endif /* MAIN_H */
