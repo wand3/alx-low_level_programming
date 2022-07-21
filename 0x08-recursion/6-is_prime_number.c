a5c656c407e3fdc6d51999a6b3cef1232cf2368a
wand3 <godswillbaiyewu@gmail.com>
2022-07-21 12:17:40 -0700

    is prime number in recursion

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+int is_divisible(int num, int div);
+
+/**
+ * is_prime_number - A function that checks if a number is prime
+ * @n: an input integer
+ * Return: 1 if n is prime or 0 if otherwise
+ */
+int is_prime_number(int n)
+{
+	int div = 2;
+
+	if (n <= 1)
+		return (0);
+	else if (n <= 3)
+		return (1);
+
+	return (is_divisible(n, div))
+}
+/**
+ * is_divisible - check if num is divisible
+ * @num: number to be checked
+ * @div: the result of division
+ * Return: 1 if num is divisible or 0 if it is not
+ */
+int is_divisible(int num, int div)
+{
+	if (num % div == 0)
+		return (0);
+	else if (div == num / 2)
+		return (1);
+
+	return (is_divisible(num, div + 1));
+}
