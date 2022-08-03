e2a9cded628d1d613acb9d09053fb6125b0fb96a
wand3 <godswillbaiyewu@gmail.com>
2022-08-02 17:42:53 -0700

    structure of dog

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef _DOG_H_
+#define _DOG_H_
+
+/**
+ * struct dog - a structure for a basic dog
+ * @name: name of dog
+ * @age: age of the dog
+ * @owner: owner of the dog
+ * Description: data structure for dog
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
