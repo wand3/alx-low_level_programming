e044cda39ab0c64d188ac6f0a0d573b170429963
wand3 <godswillbaiyewu@gmail.com>
2022-08-04 20:30:29 -0700

    print all string parameters

---
+++ 0x10-variadic_functions/2-print_strings.c
@@ -0,0 +1,35 @@
+#include <stdarg.h>
+#include <stdio.h>
+#include "variadic_functions.h"
+
+/**
+ * print_strings - function prints strings, followed by a new line
+ * @separator: the string to be printed between strings
+ * @n: the number of parameters passed to the function
+ * @...: a variable number of parameters to calculate the sum of
+ */
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	va_list strings;
+	char *str;
+	unsigned int index;
+
+	va_start(strings, n);
+
+	for (index = 0; index < n; index++)
+	{
+		str = va_arg(strings, char *);
+
+		if (str == NULL)
+			printf("(nil)");
+		else
+			printf("%s", str);
+
+		if (index != (n - 1) && separator != NULL)
+			printf("%s", separator);
+	}
+
+	printf("\n");
+
+	va_end(strings);
+}
