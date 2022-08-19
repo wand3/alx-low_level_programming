23945547e7c1caf7419bffb95a8d9b68827af5dd
wand3 <godswillbaiyewu@gmail.com>
2022-08-18 20:44:59 -0700

    password try

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -9,21 +9,23 @@
  */
 unsigned int binary_to_uint(const char *b)
 {
-	unsigned int num = 0, mul = 1;
-	int len;
+	unsigned int num = 0, mul = 1, len = 0;
 
-	if (b == '\0')
+	if (b == NULL)
 		return (0);
 
-	for (len = 0; b[len];)
+	while (b[len])
 		len++;
 
-	for (len -= 1; len >= 0; len--)
+	while (len)
 	{
 		if (b[len] != '0' && b[len] != '1')
 			return (0);
-		num += (b[len] - '0') * mul;
+
+		if (b[len] == '1')
+			num += mul;
 		mul *= 2;
+		len--;
 	}
 
 	return (num);
