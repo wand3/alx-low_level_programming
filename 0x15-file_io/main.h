1255dc7133a535770982d33deeef552668330645
wand3 <godswillbaiyewu@gmail.com>
2022-08-22 14:16:09 -0700

    creates a file

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -1,8 +1,13 @@
 #ifndef _MAIN_H_
 #define _MAIN_H_
 
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+
 int _putchar(char s);
 ssize_t read_textfile(const char *filename, size_t letters);
-
+int create_file(const char *filename, char *text_content);
 
 #endif
